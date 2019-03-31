44d1de4017a84399b040a69d5eec5856b635611a
Volodymyr Polishchuk <volodymyrpo@tahometer.com>
2019-03-31 14:02:56 +0300

    add cross origin for done controller

--- src/main/java/com/volodymyrpoli/skillrace/controller/DoneController.java
+++ src/main/java/com/volodymyrpoli/skillrace/controller/DoneController.java
@@ -17,6 +17,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("done")
+@CrossOrigin(origins = "${cross.origin}")
 public class DoneController {
 
     private final DoneService doneService;
